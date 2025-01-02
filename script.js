f487c9949b2f004b995cde9920414af4a084aee9
dwlknsn <dwlknsn@users.noreply.github.com>
2025-01-02 14:56:53 +0700

    Add form for adding new book

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-// console.log("connected")
-
-const myLibrary = [];
-
+// constructor
 function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
@@ -12,12 +9,8 @@ function Book(title, author, pages, read) {
   }
 }
 
-function addBookToLibrary(book) {
-  return myLibrary.push(book);
-}
-
 const bookData = [
-  ["The Hobbit", "J.R.R. Tolkien", "295", false],
+  ["The Hobbit", "J.R.R. Tolkien", 295, false],
   ["Dungeon Crawler Carl #1 - Dungeon Crawler Carl", "Matt Dinniman", 446, true],
   ["Dungeon Crawler Carl #2 - Carl's Doomsday Scenario", "Matt Dinniman", 364, true],
   ["Dungeon Crawler Carl #3 - The Dungeon Anarchist's Cookbook", "Matt Dinniman", 534, true],
@@ -27,19 +20,41 @@ const bookData = [
   ["Dungeon Crawler Carl #7 - This Inevitable Ruin", "Matt Dinniman", 724, false],
 ]
 
+const myLibrary = [];
+
+function addBookToLibrary(book) {
+  return myLibrary.push(book);
+}
+
 bookData.forEach(e => {
   const book = new Book(e[0], e[1], e[2], e[3]);
   addBookToLibrary(book);
 });
 
-// console.table(myLibrary);
-
 const bookshelf = document.querySelector("#bookshelf")
 
 myLibrary.forEach((book, index) => {
+  const id = `book-${index + 1}`
+  addBookToUI(book, id)
+});
+
+function addBookToUI(book, id) {
   const newDiv = document.createElement("div");
-  newDiv.id = `book-${index + 1}`;
+  newDiv.id = id;
   newDiv.classList.add("card");
+  newDiv.classList.add(book.read ? "read" : "not-read");
   newDiv.innerText = book.info();
   bookshelf.appendChild(newDiv);
-});
+}
+
+const addBookButton = document.querySelector("#add-book-button")
+const newBookForm = document.querySelector("#new-book-form")
+
+newBookForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const formData = new FormData(newBookForm);
+  const book = new Book(...formData.values());
+  addBookToLibrary(book);
+  addBookToUI(book, `book-${myLibrary.indexOf(book) + 1}`);
+  newBookForm.reset();
+})
